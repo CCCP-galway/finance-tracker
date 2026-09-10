e4e076f1787c1a8a74347ab9a30960ee2adc30c9
Jakub Zeman <ja.za1999@outlook.com>
2026-09-10 09:21:19 +0100

    added backend with generated Spring Boot files

---
+++ backend/src/test/java/ie/universityofgalway/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.universityofgalway.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
